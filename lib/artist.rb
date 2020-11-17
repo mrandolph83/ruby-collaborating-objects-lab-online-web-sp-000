4c0729d22d8fe54a86c27e89c658a8698b340135
Matt Smith <mrandolph83@gmail.com>
2020-11-17 03:22:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,53 +1,32 @@
-# responsible for either creating the artist
-# (if the artist doesn't exist in our program yet)
-#  or finding the instance of that artist
-# (if the artist does exist).
 require 'pry'
 class Artist
 
-  attr_accessor :name
-  attr_reader :songs
+  attr_accessor :name, :songs
 
   @@all = []
 
-  def initialize(artist_name)
-    @name = artist_name
-    @@all << self
+  def initialize(name)
+    @name = name
     @songs = []
-  end
-
-  def self.find_or_create_by_name(name)
-  # This class method should take the name that is passed in (remember,
-  # it will be a string), and do one of two things. Find the artist instance
-  #   that has that name or create one if it doesn't exist. Either way, the return
-  #   value of the method will be an instance of an artist with the name
-  #   attribute filled out.
-  self.find_by_name(name) || self.create_by_name(name)
-  end
-
-  def self.find_by_name(name)
-    @@all.detect do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create_by_name(name)
-    artist = self.new(name)
-
+    save
   end
 
   def add_song(song)
-    @songs << song unless @songs.include?(song)
-    song.artist = self
+    @songs << song
   end
-
-  def print_songs
-   @songs.each {|s| puts s.name }
-  end
-
 
   def self.all
     @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(name)
+# Iterates through the @@all class variable to detect whether that name
+    self.all.detect {|artist| artist.name == name } || Artist.new(name)
+
   end
 
 end
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,19 +1,28 @@
-# Parse all the filenames in the spec/fixtures
-# folder and send the filenames to the Song class
+require 'pry'
+
 class MP3Importer
 
-  attr_accessor :path
+attr_accessor :path
+# Path is your file path, in this case taking you
+# back to fixtures/mp3s
 
-  def initialize(path)
-    @path = path
-  end
+def initialize(filename)
+  @path = filename
+end
 
-  def files
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-  end
+def files
+# Dir is a class that directory streams representing
+# directories in the underlying file system. They provide
+# a variety of ways to list directories and their contents.
+  @files = Dir.entries(@path)
+  # puts @files
+# Puts is commented out above. Running puts made a list of all the
+# entries being called on. This allowed us to see that there where
+# the 4 mp3 files as well as strange files reading as "." and "..".
+# Fixed with delete_if enumerator below.
+  @files.delete_if {|file| file == "." || file == ".."}
+end
+# binding.pry
 
-  def import
-    files.each {|song| Song.new_by_filename(song)}
-  end
 
 end
