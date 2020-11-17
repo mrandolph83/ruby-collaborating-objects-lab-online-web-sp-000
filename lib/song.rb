5c14c3ccaacffc3d07e96e0dbc32e188c39cd1c3
Matt Smith <mrandolph83@gmail.com>
2020-11-17 04:45:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,17 @@ attr_accessor :name, :artist
   end
 
   def self.new_by_filename(file_name)
+  # Given how the file names are written out (ex -
+  # "Action Bronson - Larry Csonka - indie.mp3"), splitting
+  # the name by the dashes "-", and turning the filename into
+  # an array.
     split_name = filename.split(" - ")
+  # With the new split_name array made, the next step is to pull
+  # the song out of the array, which is split_name[1]
     self.new(split_name[1])
+  # Pulling the artist from the filename, wich is split_name[0]
     artist = Artist.find_or_create_by_name(split_name[0])
+
     song.artist = artist
     artist.add_song(song)
     song
