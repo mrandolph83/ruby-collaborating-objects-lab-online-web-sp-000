6bb8ab606a541775d8493f743cd238fc6d4a5322
Matt Smith <mrandolph83@gmail.com>
2020-11-17 04:24:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,10 @@ attr_accessor :name, :artist
   def self.new_by_filename(file_name)
     split_name = filename.split(" - ")
     self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
   end
 
 end
